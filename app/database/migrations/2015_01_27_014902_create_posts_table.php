1bedb18c734ecb7d13334ad9523953d6c39a8f63
nicolas mena <nmena.garzon@gmail.com>
2015-01-26 22:05:41 -0500

    Create migrations

---
+++ app/database/migrations/2015_01_27_014902_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('posts', function($table){
+			
+			$table->create();
+			$table->increments('id');
+			$table->string('title');
+			$table->string('body');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
